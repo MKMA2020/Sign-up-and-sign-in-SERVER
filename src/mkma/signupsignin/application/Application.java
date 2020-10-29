9497204423e5ace562ced4f7815b62f394f79f68
2dam <2dam@MACINF02.TartangaLH.eus>
2020-10-29 09:18:03 +0100

    Server loop working and threads finished (can receive many petitions) Missing the link with the database to treat the message received

--- src/mkma/signupsignin/application/Application.java
+++ src/mkma/signupsignin/application/Application.java
@@ -11,67 +11,37 @@ import mkma.signupsignin.thread.Worker;
 import user_message.Message;
 
 /**
- * Main for the server side. It creates a server socket that listens
- * for clients petitions and creates a thread in order to attend them.
- * It is in constant loop until manually stopped.
+ * Main for the server side. It creates a server socket that listens for clients
+ * petitions and creates a thread in order to attend them. It is in constant
+ * loop until manually stopped.
+ *
  * @author Kerman Rodríguez and Martín Gros
  */
 public class Application {
 
     /**
-     * Main method. It creates the listening server socket
-     * which then creates the needed threads.
+     * Main method. It creates the listening server socket which then creates
+     * the needed threads.
+     *
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
         //Variable declaration
         boolean loop = true;
-        while(loop){
         ServerSocket server;
+        server = new ServerSocket(6302);
         Socket service = null;
-        ObjectInputStream entry = null;
-        InputStream input = null;
-        //Opening of the server socket
-        try {
-            server = new ServerSocket(6302);
-            service = server.accept();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        
-        //Opening of the entry stream
-        try {
-            input = service.getInputStream();
-            entry = new ObjectInputStream(input);
-        } catch(IOException e) {
-            System.out.println(e);
-        }
-        //Message received and treated
-        try {           
-            Message received = (Message) entry.readObject();
-            System.out.println(received.getUser().getLogin());
-            System.out.println(received.getUser().getEmail());
-        } catch (IOException | ClassNotFoundException ex) {
-            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
-            //Closes the streams and the socket
-        }finally {
+        while (loop) {
+            //Opening of the server socket
             try {
-                input.close();
-            } catch (IOException ex) {
-                System.out.println (ex.getMessage());
+                service = server.accept();
+                Worker thread = new Worker(service);
+                new Thread(thread).start();
+            } catch (IOException e) {
+                System.out.println(e);
             }
-            try {
-                entry.close();
-            } catch (IOException ex) {
-                System.out.println (ex.getMessage());
-            }
-            try {
-                service.close();
-            } catch (IOException ex) {
-                System.out.println (ex.getMessage());
-            }           
-        }   
-        }   
+
+        }
     }
-    
 }
--- src/mkma/signupsignin/thread/Worker.java
+++ src/mkma/signupsignin/thread/Worker.java
@@ -5,14 +5,65 @@
  */
 package mkma.signupsignin.thread;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import user_message.Message;
+
 /**
  *
  * @author 2dam
  */
 public class Worker extends Thread {
 
+    private Socket service;
+
+    public Worker(Socket service) {
+        this.service = service;
+    }
+
     @Override
     public void run() {
-        
+
+        ObjectInputStream entry = null;
+        InputStream input = null;
+
+        //Opening of the entry stream
+        try {
+            input = service.getInputStream();
+            entry = new ObjectInputStream(input);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        //Message received and treated
+        try {
+            Message received = (Message) entry.readObject();
+            System.out.println(received.getUser().getLogin());
+            System.out.println(received.getUser().getEmail());
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+            //Closes the streams and the socket
+        } finally {
+            try {
+                entry.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            try {
+                input.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+            try {
+                service.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
+
 }
