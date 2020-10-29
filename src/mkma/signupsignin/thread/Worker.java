9a9b60a9ff442d9d028436d5af39b99d0e8250de
2dam <2dam@MACINF02.TartangaLH.eus>
2020-10-29 09:48:36 +0100

    Connection try with no pool

--- src/mkma/signupsignin/thread/Worker.java
+++ src/mkma/signupsignin/thread/Worker.java
@@ -5,12 +5,20 @@
  */
 package mkma.signupsignin.thread;
 
+import exceptions.DataBaseConnectionException;
+import exceptions.PassNotCorrectException;
+import exceptions.ServerErrorException;
+import exceptions.TimeOutException;
+import exceptions.UserExistsException;
+import exceptions.UserNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import mkma.signupsignin.dataaccess.SignableFactory;
+import signable.Signable;
 import user_message.Message;
 
 /**
@@ -30,6 +38,8 @@ public class Worker extends Thread {
 
         ObjectInputStream entry = null;
         InputStream input = null;
+        Message signin = new Message();
+        signin.setMessageType(1);
 
         //Opening of the entry stream
         try {
@@ -44,9 +54,28 @@ public class Worker extends Thread {
             Message received = (Message) entry.readObject();
             System.out.println(received.getUser().getLogin());
             System.out.println(received.getUser().getEmail());
+            SignableFactory factory = new SignableFactory();
+            Signable signable = factory.getSignable();
+            if (received.getMessageType() == signin.getMessageType()) 
+                signable.signIn(received.getUser());
+            else 
+                 signable.signUp(received.getUser());
+       
         } catch (IOException | ClassNotFoundException ex) {
             Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
             //Closes the streams and the socket
+        } catch (DataBaseConnectionException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (PassNotCorrectException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ServerErrorException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TimeOutException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UserNotFoundException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UserExistsException ex) {
+            Logger.getLogger(Worker.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             try {
                 entry.close();
